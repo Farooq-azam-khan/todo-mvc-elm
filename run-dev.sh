de0b4754e332fbd31768f8f02813f17feabe4dd7
Farooq <khanazamfarooq99@gmail.com>
2021-01-18 10:16:23 -0500

    tailwind dev script (remove purging)

--- run-dev.sh
+++ run-dev.sh
@@ -1,1 +1,6 @@
+ # build tailwind without any purges
+ sed -i "s/enabled: true/enabled: false/g" ./tailwind.config.js
+ tailwindcss build src/tailwind.css -o src/styles.css
+
+ # start elm development server 
  elm-live ./src/Main.elm --pushstate -- --debug --output=elm.js

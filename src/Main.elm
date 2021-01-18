91ba5a80693df4c9e477c42de59a7a1472a56c8a
Farooq <khanazamfarooq99@gmail.com>
2021-01-18 18:19:43 -0500

    elm format and if condition to show filters and actions etc.

--- src/Main.elm
+++ src/Main.elm
@@ -37,12 +37,14 @@ main =
 
 
 init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
-init flags url key =
-    ( { key = key, url = url, todos = [Todo 1 "test" False] 
-    , fetched_todos = NotFetching
-    , new_todo = ""
-    , filted_option = All 
-    }
+init _ url key =
+    ( { key = key
+      , url = url
+      , todos = [ Todo 1 "Testing Default" False ]
+      , fetched_todos = NotFetching
+      , new_todo = ""
+      , filted_option = All
+      }
     , Cmd.none
     )
 
@@ -63,27 +65,44 @@ update msg model =
 
         UrlChanged url ->
             ( { model | url = url }, Cmd.none )
-        
-        UpdateTodo str -> 
-            ({model | new_todo=str}, Cmd.none)
-        
-        CreateNewTodo -> 
-            let 
-                theTodo = Todo ((List.length model.todos) + 1) model.new_todo False 
-                newTodos = List.append model.todos [theTodo]
-            in 
-                ({model | todos = newTodos, new_todo = ""}, Cmd.none)
 
-        ChangeFilterOption filter_op -> 
-            ({model | filted_option = filter_op}, Cmd.none)
-        CompleteTodo  todo bool -> 
-            let 
-                update_todo = Todo todo.id todo.title (not todo.completed)
-                updated_todos = List.map (\aTodo -> if aTodo.id == todo.id then update_todo else aTodo) model.todos
+        UpdateTodo str ->
+            ( { model | new_todo = str }, Cmd.none )
+
+        CreateNewTodo ->
+            let
+                theTodo =
+                    Todo (List.length model.todos + 1) model.new_todo False
+
+                newTodos =
+                    List.append model.todos [ theTodo ]
             in
-                ({model | todos = updated_todos}, Cmd.none)
-        ClearCompleted -> 
-            ({model | todos = List.filter (\todo -> not todo.completed) model.todos}, Cmd.none)
+            ( { model | todos = newTodos, new_todo = "" }, Cmd.none )
+
+        ChangeFilterOption filter_op ->
+            ( { model | filted_option = filter_op }, Cmd.none )
+
+        CompleteTodo todo _ ->
+            let
+                update_todo =
+                    Todo todo.id todo.title (not todo.completed)
+
+                updated_todos =
+                    List.map
+                        (\aTodo ->
+                            if aTodo.id == todo.id then
+                                update_todo
+
+                            else
+                                aTodo
+                        )
+                        model.todos
+            in
+            ( { model | todos = updated_todos }, Cmd.none )
+
+        ClearCompleted ->
+            ( { model | todos = List.filter (\todo -> not todo.completed) model.todos }, Cmd.none )
+
         GotTodos result ->
             case result of
                 Ok d ->
@@ -93,68 +112,84 @@ update msg model =
                     ( { model | fetched_todos = Failure }, Cmd.none )
 
 
-
 view : Model -> Browser.Document Msg
 view model =
     { title = "Todo MVC"
     , body =
-        [ div [ class "bg-gray-100 flex flex-col justify-center"] [h1 [class "text-6xl opacity-50 text-red-300 text-center"] [text "todos"]
-        , Html.form [ onSubmit CreateNewTodo
-        ,class "flex items-center justify-center mt-10"] [
-            input [
-            placeholder "What needs to be done?"
-            , value model.new_todo
-            , onInput UpdateTodo
-            , class "px-10 py-2 shadow-xl"
-        ] []
-        ]
-        , todosView model.todos model.filted_option
-        , div [class "flex justify-between "] [
-            text (String.append (String.fromInt(displayCheckedItems model.todos)) " items left")
-            , div [class "flex items-center space-x-6"] [
-                 filterButton All 
-                , filterButton Active 
-                , filterButton Completed
-        ]
-        , button [onClick ClearCompleted] [text "Clear Completed"]
-        ]
-        ]
+        [ div [ class "bg-gray-100 flex flex-col justify-center" ]
+            [ h1 [ class "text-6xl opacity-50 text-red-300 text-center" ] [ text "todos" ]
+            , Html.form
+                [ onSubmit CreateNewTodo
+                , class "flex items-center justify-center mt-10"
+                ]
+                [ input
+                    [ placeholder "What needs to be done?"
+                    , value model.new_todo
+                    , onInput UpdateTodo
+                    , class "px-10 py-2 shadow-xl"
+                    ]
+                    []
+                ]
+            , if not (List.length model.todos == 0) then
+                div []
+                    [ todosView model.todos model.filted_option
+                    , div [ class "flex justify-between " ]
+                        [ text (String.append (String.fromInt (displayCheckedItems model.todos)) " items left")
+                        , div [ class "flex items-center space-x-6" ]
+                            [ filterButton All "All"
+                            , filterButton Active "Active"
+                            , filterButton Completed "Completed"
+                            ]
+                        , button [ onClick ClearCompleted ] [ text "Clear Completed" ]
+                        ]
+                    ]
+
+              else
+                div [] []
+            ]
         ]
     }
 
-filterButton : FilterOption -> Html Msg 
-filterButton filted_option = case filted_option of 
-                                All -> 
-                                    button [onClick (ChangeFilterOption All) ] [text "All"]
-                                Active -> 
-                                    button [onClick (ChangeFilterOption Active) ] [text "Active"]
-                                Completed -> 
-                                    button [onClick (ChangeFilterOption Completed) ] [text "Completed"]
 
-getUnCompletedTodos : List Todo -> List Todo 
-getUnCompletedTodos = List.filter (\todo -> not todo.completed)
+filterButton : FilterOption -> String -> Html Msg
+filterButton filted_option filter_text =
+    button
+        [ onClick (ChangeFilterOption filted_option)
+        , class ""
+        ]
+        [ text filter_text ]
+
+
+getUnCompletedTodos : List Todo -> List Todo
+getUnCompletedTodos =
+    List.filter (\todo -> not todo.completed)
+
 
 displayCheckedItems : List Todo -> Int
-displayCheckedItems todos = List.length (getUnCompletedTodos todos)
+displayCheckedItems todos =
+    List.length (getUnCompletedTodos todos)
 
 
 todosView : List Todo -> FilterOption -> Html Msg
 todosView todos filtered_options =
-    case filtered_options of 
-        All -> 
+    case filtered_options of
+        All ->
             ul [] (List.map showTodo todos)
-        Active -> 
+
+        Active ->
             ul [] (List.map showTodo (List.filter (\todo -> not todo.completed) todos))
-        Completed -> 
+
+        Completed ->
             ul [] (List.map showTodo (List.filter (\todo -> todo.completed) todos))
 
 
 showTodo : Todo -> Html Msg
 showTodo todo =
-    li [] [ 
-        input [type_ "checkbox", onCheck (CompleteTodo todo), checked todo.completed] []
+    li []
+        [ input [ type_ "checkbox", onCheck (CompleteTodo todo), checked todo.completed ] []
         , text todo.title
-         ]
+        ]
+
 
 
 -- viewLink : String -> Html Msg
